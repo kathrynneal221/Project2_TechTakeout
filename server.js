8a634e9b869cc4110ec9c94b4829c62cf1edca5f
Duane Cantera <duane.cantera@comcast.net>
2022-01-04 18:35:05 -0500

    Made changes to get the server to run.

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+
+// Commented this out.  Don't need it.
+// Don't have helpers.js file in utils directory.
+//const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -11,7 +14,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
+// Commented out not using file helpers.js in utils directory.
+//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
