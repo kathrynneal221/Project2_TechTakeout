fb57ed317fe637add3a3a2f451f92494178526e3
Duane Cantera <duane.cantera@comcast.net>
2022-01-04 20:41:57 -0500

    added menu route.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const menuRoutes = require('./menuRoutes');
 
 router.use('/users', userRoutes);
+router.use('/menus', menuRoutes);
 
 module.exports = router;
