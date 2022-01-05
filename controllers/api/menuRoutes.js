545e2e159191ea52f712a4e1d09c4033a4d55c38
Duane Cantera <duane.cantera@comcast.net>
2022-01-04 20:43:18 -0500

    Added menu routes.

---
+++ controllers/api/menuRoutes.js
@@ -0,0 +1,84 @@
+const router = require('express').Router();
+const { Restaurant, Menu } = require('../../models');
+
+// GET all menus for menu view.
+router.get('/', async (req, res) => {
+  try 
+  {
+    const dbMenuData = await Menu.findAll({
+      include: [{ model: Restaurant }],
+    });
+
+    const menus = dbMenuData.map((project) => project.get({ plain: true }));
+
+    res.render('menu', {menus, loggedIn: req.session.loggedIn, });
+  } 
+  catch (err) 
+  {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+// // GET one restaurant
+// router.get('/restaurant/:id', async (req, res) => {
+//   // If the user is not logged in, redirect the user to the login page
+//   if (!req.session.loggedIn) {
+//     res.redirect('/login');
+//   } else {
+//     // If the user is logged in, allow them to view the gallery
+//     try {
+//       const dbGalleryData = await Gallery.findByPk(req.params.id, {
+//         include: [
+//           {
+//             model: Painting,
+//             attributes: [
+//               'id',
+//               'title',
+//               'artist',
+//               'exhibition_date',
+//               'filename',
+//               'description',
+//             ],
+//           },
+//         ],
+//       });
+//       const gallery = dbGalleryData.get({ plain: true });
+//       res.render('gallery', { gallery, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   }
+// });
+
+// // GET one painting
+// router.get('/painting/:id', async (req, res) => {
+//   // If the user is not logged in, redirect the user to the login page
+//   if (!req.session.loggedIn) {
+//     res.redirect('/login');
+//   } else {
+//     // If the user is logged in, allow them to view the painting
+//     try {
+//       const dbPaintingData = await Painting.findByPk(req.params.id);
+
+//       const painting = dbPaintingData.get({ plain: true });
+
+//       res.render('painting', { painting, loggedIn: req.session.loggedIn });
+//     } catch (err) {
+//       console.log(err);
+//       res.status(500).json(err);
+//     }
+//   }
+// });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
