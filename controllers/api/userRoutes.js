fb40758219ea9a41632c67d6e1d1610505b0eea4
Kathryn Neal <kati.neal221@gmail.com>
2022-01-08 13:32:37 -0500

    fixed styling

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,5 @@
 const router = require('express').Router();
 const passport = require('passport');
-// const Strategy = require('passport-local');
-// const crypto = require('crypto');
 
 const { User } = require('../../models');
 
--- public/js/login.js
+++ public/js/login.js
@@ -13,13 +13,5 @@ async function loginFormHandler(event) {
       }),
       headers: { 'Content-Type': 'application/json' }
     });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
   }
 }
-
-// document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
